cb0f7bd02760827188c7afc0382d50f7cf5e7b7a
Francisco Fuster Andujar <frafusan@ei.upv.es>
2015-03-21 20:20:05 +0100

    Modificaciones tras cambio de tamaño del canvas del map y mejora de la carga del mapa

--- www/js/googlemaps.js
+++ www/js/googlemaps.js
@@ -1,68 +1,70 @@
 
- $
-            var map,
-                id,
-                currentPosition,
-                mapCenter = new google.maps.LatLng(39.473263, -0.383927);
-            function initialize() {
-                alert("entro");
-                intitilizeMap();
-                if(navigator.geolocation) {
-                    id = navigator.geolocation.getCurrentPosition ( displayAndWatchMap, locationError );
-                }else{
-                    alert("Se ha producido un error");
-                }
-            }
-            
-            // Inicilizamos el mapa
-            function intitilizeMap(){ 
-                alert("aqui llego?")
-                 var mapOptions = {
-                    zoom: 16,
-                    center: mapCenter,
-                     mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                map = new google.maps.Map(document.getElementById('map-canvas'),
-                                          mapOptions);
-                
-                alert("aqui llego2?")
-            }
-            
-            // En caso de error al obtener la posicion mostramos un mensaje
-            function locationError(){ 
-                alert("Imposible obtener la localización");
-            }
-            
-            // Establecemos la posición actual en el mapa
-            function setPositionInGoogleMap(position){ 
-                var mapPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-               
 
-                currentPosition = new google.maps.Marker({
-                    position: mapPos,
-                    map: map,
-                    title: "Posición actual"
-                });
-                
-                map.panTo(mapPos)
-            }
-            
-            // Obtenemos la posicion actual
-            function getCurrentPosition(){
-                id = navigator.geolocation.watchPosition (
-                    function (position){
-                        setMarker(currentPosition, position);
-                    });
-            }
-            
-            function setMarker (marker, position){
-                var mapPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                marker.setPosition(mapPos);
-            }
-            
-            function displayAndWatchMap(position){
-                setPositionInGoogleMap(position);
-                getCurrentPosition();
-            }
-            //google.maps.event.addDomListener(window, 'load', initialize);
-            
+var map,
+    id,
+    currentPosition,
+    mapCenter = new google.maps.LatLng(39.473263, -0.383927);
+function initialize() {
+    intitilizeMap();
+    resize();
+    if(navigator.geolocation) {
+        id = navigator.geolocation.getCurrentPosition ( displayAndWatchMap, locationError );
+    }else{
+        alert("Se ha producido un error");
+    }
+}
+
+// Inicilizamos el mapa
+function intitilizeMap(){ 
+    var mapOptions = {
+        zoom: 16,
+        center: mapCenter,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+                              mapOptions);
+
+    }
+
+// En caso de error al obtener la posicion mostramos un mensaje
+function locationError(){ 
+    alert("Imposible obtener la localización");
+}
+
+// Establecemos la posición actual en el mapa
+function setPositionInGoogleMap(position){ 
+    var mapPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+
+    currentPosition = new google.maps.Marker({
+        position: mapPos,
+        map: map,
+        title: "Posición actual"
+    });
+
+    map.panTo(mapPos)
+}
+
+// Obtenemos la posicion actual
+function getCurrentPosition(){
+    id = navigator.geolocation.watchPosition (
+        function (position){
+            setMarker(currentPosition, position);
+        });
+}
+
+function setMarker (marker, position){
+    var mapPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    marker.setPosition(mapPos);
+}
+
+function displayAndWatchMap(position){
+    setPositionInGoogleMap(position);
+    getCurrentPosition();
+}
+function resize() {
+    var alto_boton = $('#cerrar').height();
+    var alto_pantalla = $(window).height();
+    $("#map-canvas").height(alto_pantalla -alto_boton);
+}
+//google.maps.event.addDomListener(window, 'load', initialize);
