75e42e69e2378c64b60d43ec784101cb644a2406
Francisco Fuster Andujar <frafusan@ei.upv.es>
2015-04-20 22:35:32 +0200

    Eliminados archivos de platforms que no deben estar en el repositorio

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/frafusan/proyectos/imagetopast/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/frafusan/proyectos/imagetopast/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/frafusan/proyectos/imagetopast/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/frafusan/proyectos/imagetopast/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/frafusan/proyectos/imagetopast/platforms/android/ant-build/classes.dex \
-/Users/frafusan/proyectos/imagetopast/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/frafusan/proyectos/imagetopast/platforms/android/libs/wikitudesdk.jar \
